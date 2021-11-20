7fb37f93b145fb1299f982c9f26827478ce8f617
Mateo Paredes Sepulveda <mateopase@gmail.com>
2021-11-19 23:16:46 -0500

    Twilio SDK

--- web/main.py
+++ web/main.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/sms")
+@app.route("/sms/reply", methods=["POST"])
 def handle_sms():
     return "Hello"
