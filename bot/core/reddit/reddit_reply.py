c12c54a913d1ed5ebbd9b492cb47e968e8caa876
Mateo Paredes Sepulveda <mateopase@gmail.com>
2021-11-22 22:25:45 -0500

    Added buttons finally phew

--- bot/core/reddit/reddit_reply.py
+++ bot/core/reddit/reddit_reply.py
@@ -36,8 +36,28 @@ def build_subreddit_reply(sub: str, page: int, sort: str, time: str) -> tuple[li
 
         header = [f"Subreddit: /r/{sub.title()} | Page: {page}"]
         messages = header + (display_names or [f"No more posts :'("])
+        buttons = [
+            {
+                "content_type": "text",
+                "title": f"{post.title[:17]}...",
+                "payload": f"reddit post {post.id}",
+            } 
+        for post in posts.items]
+        if page != 1:
+            buttons += [{
+                "content_type": "text",
+                "title": "Previous Page",
+                "payload": f"reddit subreddit {sub} -p {page} -s {sort} -t {time}",
+            }]
+        if posts.has_more:
+            next_page = page + 1
+            buttons += [{
+                "content_type": "text",
+                "title": "Next Page",
+                "payload": f"reddit subreddit {sub} -p {next_page} -s {sort} -t {time}",
+            }]
 
-        return messages , None
+        return messages, buttons
     else:
         return [f"Error: {sub} is not a sub or Reddit is having issues :'("], None
 
@@ -47,7 +67,19 @@ def build_post_reply(post_id: str) -> tuple[list[str], list[dict]]:
     if post:
         header = [f"Title: {post.title} | ID: {post.id} | /r/{post.subreddit.display_name} | /u/{post.author.name} | {post.score}pts | {post.num_comments}cmnts"]
         messages = header + ([post.selftext] if post.selftext else ["No post body or this is an image post :'("])
-        return messages, None
+        buttons = [
+            {
+                "content_type": "text",
+                "title": f"Back to posts (by hot)",
+                "payload": f"reddit subreddit {post.subreddit.display_name}",
+            },
+            {
+                "content_type": "text",
+                "title": f"Load comments",
+                "payload": f"reddit comments {post_id}",
+            }
+        ]
+        return messages, buttons
     else:
         return [f"Error: {post_id} is not a valid post ID or Reddit is having issues :'("], None
 
@@ -59,6 +91,27 @@ def build_comment_reply(post_id: str, page: int, sort: str) -> tuple[list[str], 
         header = [f"Title: {post.title} | ID: {post.id} | /r/{post.subreddit.display_name} | /u/{post.author.name} | {post.score}pts | {post.num_comments}cmnts"]
         comments = [f"/u/{comment.author} | {comment.score}pts: {comment.body}" for comment in post_comments.items]
         messages = header + (comments or ["No more comments :'("])
-        return messages, None
+        buttons = [
+            {
+                "content_type": "text",
+                "title": f"Back to post",
+                "payload": f"reddit post {post.id}",
+            } 
+        ]
+
+        if page != 1:
+            buttons += [{
+                "content_type": "text",
+                "title": "Previous Page",
+                "payload": f"reddit comments {post_id} -p {page} -s {sort}",
+            }]
+        if post_comments.has_more:
+            next_page = page + 1
+            buttons += [{
+                "content_type": "text",
+                "title": "Next Page",
+                "payload": f"reddit comments {post_id} -p {next_page} -s {sort}",
+            }]
+        return messages, buttons
     else:
         return [f"Error: {post_id} is not a valid post ID or Reddit is having issues :'("], None
